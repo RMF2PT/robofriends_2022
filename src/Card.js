fe0a7081be1a944edec019d31143baeb384e8fd0
rmf2pt <ricardo@Ricardos-iMac.local>
2021-11-25 20:01:24 +0000

    Add card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <img alt='robot' src='https://robohash.org/rmf2pt' />
+      <div>
+        <h2>Joe Doe</h2>
+        <p>joe.doe@gmail.com</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card;
