80e5a3840ad2d6055a4fa7fcb9d8ae8ba2e41603
rmf2pt <rmf2pt@gmail.com>
2021-12-02 00:00:03 +0000

    Fix some styling

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
     return (
       <div className='bg-gradient-to-r from-indigo-400 to-green-800 w-full h-screen'>
         <div className='flex flex-col items-center'>
-          <h1 className='uppercase text-4xl p-2 text-green-400'>Robofriends</h1>
+          <h1 className='uppercase text-4xl p-2 text-green-400 mt-4'>Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
         </div>
         <CardList robots={filteredRobots} />
