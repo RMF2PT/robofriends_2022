63dc730230205c185adbc657d5fdff55ec76388b
rmf2pt <rmf2pt@gmail.com>
2021-12-02 13:57:34 +0000

    Use fetch via API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import { robots } from './robots'
+// import { robots } from './robots'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: '',
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => {this.setState({ robots: users})});
   }
 
   onSearchChange = (event) => {
@@ -20,15 +26,19 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     });
-    return (
-      <div className='bg-gradient-to-r from-indigo-400 to-green-800 w-full h-screen'>
+    if (this.state.robots.length === 0) {
+      return <h1>Loading</h1>
+    } else {
+      return (
+        <div className='bg-gradient-to-r from-indigo-400 to-green-800 w-full h-screen'>
         <div className='flex flex-col items-center'>
           <h1 className='uppercase text-4xl p-2 text-green-400 mt-4'>Robofriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
         </div>
         <CardList robots={filteredRobots} />
       </div>
-    );
+      );
+    }
   }
 }
 
